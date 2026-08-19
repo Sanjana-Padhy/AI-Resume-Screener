a53b18989719c3082c8df259e309c496cd6bfff8
Sanjana Padhy <sanjanapadhy867@gmail.com>
2026-08-19 13:32:52 +0530

    Add resume upload form with view, template, and URL routing

--- screener/views.py
+++ screener/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CandidateForm
 
-# Create your views here.
+def upload_resume(request):
+    if request.method == 'POST':
+        form = CandidateForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('upload_success')
+    else:
+        form = CandidateForm()
+
+    return render(request, 'screener/upload.html', {'form': form})
+
+
+def upload_success(request):
+    return render(request, 'screener/success.html')
